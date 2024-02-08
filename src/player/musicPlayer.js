e79733eaafd7b46b9cf86c93aa0988dff3b3c212
Niko Dovidija <niko@diligentstudios.com>
2024-02-08 15:32:34 +0100

    project setup

---
+++ src/player/musicPlayer.js
@@ -0,0 +1,41 @@
+// import { createNoise2D } from 'simplex-noise'
+// import * as THREE from 'three'
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+// const musicContainer = document.getElementById('music-container')
+// const playBtn = document.getElementById('play')
+// const prevBtn = document.getElementById('prev')
+// const nextBtn = document.getElementById('next')
+// const audio = document.getElementById('audio')
+// const progress = document.getElementById('progress')
+// const progressContainer = document.getElementById('progress-container')
+// const title = document.getElementById('title')
+// const currTime = document.querySelector('#currTime')
+// const playIcon = document.querySelector('.play-img')
+// const pauseIcon = document.querySelector('.pause-img')
+// const vibingEmoji = document.querySelector('.vibing-emoji')
+// const staticEmoji = document.querySelector('.static-emoji')
+
+//get all the songs from the /music folder
+
+// const songs = [
+//   {
+//     name: 'song 1',
+//     url: 'captain_zero.mp3',
+//   },
+//   {
+//     name: 'song 2',
+//     url: 'remember_when.mp3',
+//   },
+//   {
+//     name: 'song 3',
+//     url: 'temple_of_the_moon.mp3',
+//   },
+// ]
+
+function initMusicPlayer() {
+  // Keep track of song
+  //let songIndex = 2
+}
+
+export default initMusicPlayer
