c4324e41cb8f7e17069499ec6d7547cc6c7e82ff
Niko Dovidija <niko@diligentstudios.com>
2024-02-08 18:38:57 +0100

    player fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import initMusicPlayer from './player/musicPlayer'
 import './styles/style.css'
 
-initMusicPlayer()
+document.addEventListener('DOMContentLoaded', () => {
+  let audioContext
+  let analyser
+  let src
+  const audio = document.getElementById('audio')
+
+  initMusicPlayer()
+  const interactionListener = () => {
+    if (audioContext) {
+      audioContext.resume()
+    } else {
+      audioContext = new AudioContext()
+      src = audioContext.createMediaElementSource(audio)
+      analyser = audioContext.createAnalyser()
+      src.connect(analyser)
+      analyser.connect(audioContext.destination)
+      analyser.fftSize = 512
+    }
+    document.body.removeEventListener('click', interactionListener)
+  }
+  document.body.addEventListener('click', interactionListener)
+})
--- src/player/musicPlayer.js
+++ src/player/musicPlayer.js
@@ -1,41 +1,159 @@
-// import { createNoise2D } from 'simplex-noise'
+// import SimplexNoise from 'simplex-noise'
 // import * as THREE from 'three'
 // import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-// const musicContainer = document.getElementById('music-container')
-// const playBtn = document.getElementById('play')
-// const prevBtn = document.getElementById('prev')
-// const nextBtn = document.getElementById('next')
-// const audio = document.getElementById('audio')
-// const progress = document.getElementById('progress')
-// const progressContainer = document.getElementById('progress-container')
-// const title = document.getElementById('title')
-// const currTime = document.querySelector('#currTime')
-// const playIcon = document.querySelector('.play-img')
-// const pauseIcon = document.querySelector('.pause-img')
-// const vibingEmoji = document.querySelector('.vibing-emoji')
-// const staticEmoji = document.querySelector('.static-emoji')
+const musicContainer = document.getElementById('music-container')
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+const audio = document.getElementById('audio')
+const progress = document.getElementById('progress')
+const progressContainer = document.getElementById('progress-container')
+const title = document.getElementById('title')
+const currTime = document.querySelector('#currTime')
+const playIcon = document.querySelector('.play-img')
+const pauseIcon = document.querySelector('.pause-img')
 
-//get all the songs from the /music folder
-
-// const songs = [
-//   {
-//     name: 'song 1',
-//     url: 'captain_zero.mp3',
-//   },
-//   {
-//     name: 'song 2',
-//     url: 'remember_when.mp3',
-//   },
-//   {
-//     name: 'song 3',
-//     url: 'temple_of_the_moon.mp3',
-//   },
-// ]
+const songs = [
+  {
+    name: 'song 1',
+    fileName: 'captain_zero.mp3',
+  },
+  {
+    name: 'song 2',
+    fileName: 'remember_when.mp3',
+  },
+  {
+    name: 'song 3',
+    fileName: 'temple_of_the_moon.mp3',
+  },
+]
 
 function initMusicPlayer() {
-  // Keep track of song
-  //let songIndex = 2
+  const cdnPrefix =
+    'https://cdn.jsdelivr.net/gh/diligent-ground-zero/the-wave@main/music/'
+  let songIndex = 1
+  const initialSong = songs[songIndex]
+
+  if (!title || !audio) return
+
+  loadSong(initialSong)
+
+  // buttons for music player
+  prevBtn.addEventListener('click', prevSong)
+  nextBtn.addEventListener('click', nextSong)
+  // Time/song update
+  audio.addEventListener('timeupdate', updateProgress)
+  // Click on progress bar
+  progressContainer.addEventListener('click', setProgress)
+  // Song ends
+  audio.addEventListener('ended', nextSong)
+  // Time of song
+  audio.addEventListener('timeupdate', DurTime)
+
+  // load song
+  function loadSong(song) {
+    title.innerText = song.name
+    audio.src = cdnPrefix + song.fileName
+  }
+  // Play song
+  function playSong() {
+    musicContainer.classList.add('play')
+    playIcon.style.display = 'none'
+    pauseIcon.style.display = 'block'
+    audio.play()
+  }
+  // Pause song
+  function pauseSong() {
+    musicContainer.classList.remove('play')
+    playIcon.style.display = 'block'
+    pauseIcon.style.display = 'none'
+    audio.pause()
+  }
+  // Previous song
+  function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+      songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+  }
+  // Next song
+  function nextSong() {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+      songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+  }
+  // Update progress bar
+  function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+  }
+  // Set progress bar
+  function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+  }
+  //get duration & currentTime for Time of song
+  function DurTime(e) {
+    const { duration, currentTime } = e.srcElement
+    var sec
+    var sec_d
+    // define minutes currentTime
+    let min = currentTime == null ? 0 : Math.floor(currentTime / 60)
+    min = min < 10 ? '0' + min : min
+    // define seconds currentTime
+    function get_sec(x) {
+      if (Math.floor(x) >= 60) {
+        for (var i = 1; i <= 60; i++) {
+          if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+            sec = Math.floor(x) - 60 * i
+            sec = sec < 10 ? '0' + sec : sec
+          }
+        }
+      } else {
+        sec = Math.floor(x)
+        sec = sec < 10 ? '0' + sec : sec
+      }
+    }
+    get_sec(currentTime, sec)
+    // change currentTime DOM
+    currTime.innerHTML = min + ':' + sec
+    // define minutes duration
+    //let min_d = isNaN(duration) === true ? '0' : Math.floor(duration / 60)
+    //todo what dis
+    // min_d = min_d < 10 ? '0' + min_d : min_d
+    function get_sec_d(x) {
+      if (Math.floor(x) >= 60) {
+        for (var i = 1; i <= 60; i++) {
+          if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+            sec_d = Math.floor(x) - 60 * i
+            sec_d = sec_d < 10 ? '0' + sec_d : sec_d
+          }
+        }
+      } else {
+        sec_d = isNaN(duration) === true ? '0' : Math.floor(x)
+        sec_d = sec_d < 10 ? '0' + sec_d : sec_d
+      }
+    }
+    get_sec_d(duration)
+  }
+  // Event listeners
+  playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+    if (isPlaying) {
+      pauseSong()
+    } else {
+      playSong()
+    }
+  })
 }
 
 export default initMusicPlayer
