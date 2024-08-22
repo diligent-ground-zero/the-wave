55131f06e2ce8e24493790c80f1db00aee45f86f
blazserdtt <blaz@diligentstudios.com>
2024-08-22 11:02:36 +0200

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,6 @@ export default defineConfig({
       output: {
         format: 'esm',
         entryFileNames: 'main.js',
-        esModule: false,
         compact: true,
         globals: {
           jquery: '$',
