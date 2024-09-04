3f21b8575086e5a0411cd460a87b99ec86165019
Niko Dovidija <niko@diligentstudios.com>
2024-09-04 14:28:57 +0200

    up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,21 @@
-import { setupAboutUs } from './about-us/aboutUs'
 import initMusicPlayer from './player/musicPlayer'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const currentPath = window.location.pathname
-
-  if (currentPath === '/') {
-    const { ensureAudioContext, playSong, pauseSong } = initMusicPlayer()
-
-    const playButtonListener = (event) => {
-      if (event.target.closest('.play-btn')) {
-        ensureAudioContext()
-          .resume()
-          .then(() => {
-            const musicContainer = document.getElementById('music-container')
-            const isPlaying = musicContainer.classList.contains('play')
-            if (isPlaying) {
-              pauseSong()
-            } else {
-              playSong()
-            }
-          })
-      }
+  const { ensureAudioContext, playSong, pauseSong } = initMusicPlayer()
+  const playButtonListener = (event) => {
+    if (event.target.closest('.play-btn')) {
+      ensureAudioContext()
+        .resume()
+        .then(() => {
+          const musicContainer = document.getElementById('music-container')
+          const isPlaying = musicContainer.classList.contains('play')
+          if (isPlaying) {
+            pauseSong()
+          } else {
+            playSong()
+          }
+        })
     }
-
-    document.addEventListener('click', playButtonListener)
-  } else if (currentPath === '/uber-uns' || currentPath === '/uber-uns-copy') {
-    setupAboutUs()
   }
+  document.addEventListener('click', playButtonListener)
 })
